7af7b4fa8f9fbcea28bd1a65d03fc4b11d902a6d
Yelyzaveta Hrybniak <Yelyzaveta_Hrybniak@epam.com>
2020-02-03 13:46:45 +0200

    added debounce to the search bar

--- Blog/ClientApp/src/app/posts/posts.component.ts
+++ Blog/ClientApp/src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Renderer } from '@angular/core';
+import { Component, OnInit, Input, Renderer, ViewChild, ElementRef } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Post } from '../models/post';
 import { HttpParams } from '@angular/common/http';
@@ -6,8 +6,8 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { ApiPaths } from '../app.constants'
 import { PostService } from '../services/post.service';
 import { error } from 'protractor';
-import { catchError } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { catchError, filter, debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+import { Observable, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
@@ -15,6 +15,8 @@ import { Observable } from 'rxjs';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+  @ViewChild('input', { static: true }) input: ElementRef;
+
   posts: Post[] = [];
   search: string = "";
   mode: string = "text";
@@ -27,8 +29,15 @@ export class PostsComponent implements OnInit {
   constructor(private dataService: DataService, private route: ActivatedRoute, private postService: PostService, private rendered: Renderer) { }
 
   ngOnInit() {
-    
-    this.getPosts();
+    this.dataService.getItems<Post>(ApiPaths.Posts).subscribe((res) => {
+      this.posts = res.slice((this.pageIndex - 1) * this.itemsPerPage, this.itemsPerPage * this.pageIndex - 1);
+      this.postsCount = this.posts.length;
+      this.loading = false;
+      this.error_message = '';
+    },
+      (error) => {
+        this.error_message = "Sorry, couldn't find any posts!";
+      });
     this.route.queryParams.subscribe((params: Params) => {
       if (!isNaN(params['pageIndex'])) {
         this.pageIndex = params['pageIndex'];
@@ -41,45 +50,58 @@ export class PostsComponent implements OnInit {
   }
 
   getPosts(): void {
-    if (this.search === "")
-    {
-      this.dataService.getItems<Post>(ApiPaths.Posts).subscribe((res) => {
-      this.posts = res.slice((this.pageIndex-1)*this.itemsPerPage, this.itemsPerPage*this.pageIndex-1);
-      this.postsCount = this.posts.length;
-      this.loading = false;
-      this.error_message = '';
-    },
-    (error) => {
-      this.error_message = "Sorry, couldn't find any posts!";
-    });}
-    else if(this.mode === "text"){
-      this.loading = true;
-      this.postService.getPostsWithTextFilter(this.search).subscribe((res) => {
-        if (res.length != 0){
-        this.posts = res.slice((this.pageIndex-1)*this.itemsPerPage, this.itemsPerPage*this.pageIndex-1);
-        this.postsCount = this.posts.length;
-        this.error_message = '';
-        }
-        else {this.posts = null}
-        this.loading = false;
-      }, 
-      (error) => {
-        this.error_message = "Sorry, couldn't find any posts with this tegs!";
-      })
-    }
-    else if(this.mode === "teg"){
-      this.postService.getPostsWithTegFilter(this.search).subscribe((res) => {
-        if (res.length != 0){
-        this.posts = res.slice((this.pageIndex-1)*this.itemsPerPage, this.itemsPerPage*this.pageIndex-1);
-        this.postsCount = this.posts.length;
-        this.error_message = '';
-        }
-        else {this.posts = null}
-      }, 
-        (error) => {
-          this.error_message = "Sorry, couldn't find any posts with this tegs!";
+    fromEvent(this.input.nativeElement, 'keydown')
+      .pipe(
+        filter(Boolean),
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap((text) => {
+          console.log(this.input.nativeElement.value)
         })
-    }
+      )
+      .subscribe(() => {
+        this.search == this.input.nativeElement.value;
+        if (this.search === "") {
+          this.dataService.getItems<Post>(ApiPaths.Posts).subscribe((res) => {
+            this.posts = res.slice((this.pageIndex - 1) * this.itemsPerPage, this.itemsPerPage * this.pageIndex - 1);
+            this.postsCount = this.posts.length;
+            this.loading = false;
+            this.error_message = '';
+          },
+            (error) => {
+              this.error_message = "Sorry, couldn't find any posts!";
+            });
+        }
+        else if (this.mode === "text") {
+          console.log(this.search)
+          this.loading = true;
+          this.postService.getPostsWithTextFilter(this.search).subscribe((res) => {
+            if (res.length != 0) {
+              this.posts = res.slice((this.pageIndex - 1) * this.itemsPerPage, this.itemsPerPage * this.pageIndex - 1);
+              this.postsCount = this.posts.length;
+              this.error_message = '';
+            }
+            else { this.posts = null }
+            this.loading = false;
+          },
+            (error) => {
+              this.error_message = "Sorry, couldn't find any posts with this tegs!";
+            })
+        }
+        else if (this.mode === "teg") {
+          this.postService.getPostsWithTegFilter(this.search).subscribe((res) => {
+            if (res.length != 0) {
+              this.posts = res.slice((this.pageIndex - 1) * this.itemsPerPage, this.itemsPerPage * this.pageIndex - 1);
+              this.postsCount = this.posts.length;
+              this.error_message = '';
+            }
+            else { this.posts = null }
+          },
+            (error) => {
+              this.error_message = "Sorry, couldn't find any posts with this tegs!";
+            })
+        }
+      });
   }
 
   onPostDeleted(postId: number): void {
